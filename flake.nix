3473bb0854fe30b9a9bb825fbcd965167dd8b231
leningkoei <zhanglening2001@gmail.com>
2025-10-18 12:17:44 +0900

    Add host  and upgrade flake and home-manager from stable(25.05) to unstable

--- flake.nix
+++ flake.nix
@@ -2,15 +2,32 @@
   description = "NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/master";
+      # url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs = { nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations = {
+
+      shen-zhou-pc = let
+        username = "lening";
+        homepath = "../../home";
+        specialArgs = { inherit username; inherit homepath; };
+      in nixpkgs.lib.nixosSystem {
+        inherit specialArgs;
+        modules = [
+          ./host/shen-zhou-pc/configuration.nix
+          home-manager.nixosModules.home-manager {
+            home-manager.extraSpecialArgs = inputs // specialArgs;
+            home-manager.users.${username} = import ./users/${username}/home.nix;
+          }
+        ];
+      };
+
       nixos = let
         username = "lening";
         homepath = "../../home";
