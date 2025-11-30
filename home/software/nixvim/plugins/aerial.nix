1b02f64888f35e5510529dc2b7784cf4b3b75e00
leningkoei <zhanglening2001>
2025-11-30 15:05:46 +0900

    Modify the config of aerial nixvim plugin, nothing effected maybe.

--- home/software/nixvim/plugins/aerial.nix
+++ home/software/nixvim/plugins/aerial.nix
@@ -2,17 +2,43 @@
 {
   enable = true;
   settings = {
-    default_direction = "prefer_right";
+    # default_direction = "prefer_right";
     open_automatic = true;
-    placement = "edge";
+    # placement = "edge";
     show_guides = true;
     layout = {
       max_width = 36;
       min_width = 36;
     };
     filter_kind = [
-      "Class" "Constructor" "Enum" "Function" "Interface" "Module"
-      "Method" "Namespace" "Struct"
+      "Array"
+      "Boolean"
+      "Class"
+      "Constant"
+      "Constructor"
+      "Enum"
+      "EnumMember"
+      "Event"
+      "Field"
+      "File"
+      "Function"
+      "Interface"
+      "Key"
+      "Method"
+      "Module"
+      "Namespace"
+      "Null"
+      "Number"
+      "Object"
+      "Operator"
+      "Package"
+      "Property"
+      "String"
+      "Struct"
+      "TypeParameter"
+      "Variable"
+      # "Class" "Constructor" "Enum" "Function" "Interface" "Module"
+      # "Method" "Namespace" "Struct"
     ];
   };
 }
